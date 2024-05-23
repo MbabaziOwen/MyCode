7eccb854ec1a18a70a41ebbe348680f32ce21b99
MbabaziOwen <163897840+MbabaziOwen@users.noreply.github.com>
2024-05-23 13:25:42 +0300

    Add files via upload
    fizzbuzz

---
+++ fizzbuzz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(void){
+    /*multiples of 3, print Buzz
+      multiple of five, print Buzz
+      multiple of 7, print buzz
+      if its not any of them print Fuzz and print the number
+       */
+      int number = printf("Give me a number: ");
+      scanf("%d",&number);
+
+
+      if(number %7==0){
+            printf("Buzz");
+      }else if (number %5==0){
+        printf("buzz");
+      }else if (number %3==0){
+        printf("buzz");
+      }else {
+        printf("Fuzz%d",number);
+      }
+            
+
+                
+        
+        return 0;
+}
